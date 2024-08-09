9c76fb7ef7a7f832e119a3017f3ee5d1b3cc370a
Mark Hu <mark.hu@asml.com>
2024-08-09 17:03:07 +0200

    yes

--- src/matlab/private/environmentinstantiate.m
+++ src/matlab/private/environmentinstantiate.m
@@ -4,7 +4,7 @@ function environmentinstantiate(juliaexe, environment)
         environment (1,1) string
     end
 
-    [status, output] = shell(juliaexe, ['--project="', char(environment), '"'], '-e', '"import Pkg; Pkg.instantiate()"');
+    [status, ~] = shell(juliaexe, ['--project="', char(environment), '"'], '-e', 'import Pkg; Pkg.instantiate()');
 
     assert(~status, "matfrostjulia:exe", "Could not instantiate environment: " + environment);
 
