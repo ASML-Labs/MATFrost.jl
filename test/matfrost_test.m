84b0c29df2b3af70f56351da7f5a940c2b210185
Joris Belier <joris.belier@asml.com>
2024-08-12 11:23:44 +0200

    Test?

--- test/matfrost_test.m
+++ test/matfrost_test.m
@@ -5,16 +5,16 @@ classdef matfrost_test < matfrost_abstract_test
     methods(Test)
         % Test methods
         
-        function simple_numerical_tests(tc)            
-            tc.verifyEqual(...
-                tc.mjl.MATFrostTest.elementwise_addition_f64(100, (1:100)'), ...
-                100.+(1:100)');
-
-            tc.verifyEqual(...
-                tc.mjl.MATFrostTest.kron_product_matrix_f64(diag(1:10), (1:10)'*(1:10)), ...
-                kron(diag(1:10), (1:10)'*(1:10)));
-
-        end
+%         function simple_numerical_tests(tc)            
+%             tc.verifyEqual(...
+%                 tc.mjl.MATFrostTest.elementwise_addition_f64(100, (1:100)'), ...
+%                 100.+(1:100)');
+% 
+%             tc.verifyEqual(...
+%                 tc.mjl.MATFrostTest.kron_product_matrix_f64(diag(1:10), (1:10)'*(1:10)), ...
+%                 kron(diag(1:10), (1:10)'*(1:10)));
+% 
+%         end
 % 
 %         function sum_vector_of_vector_f64(tc)
 %             vov = cell(10,1);
