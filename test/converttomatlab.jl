137c420030b3af972eef421c21cb4f93611bae5c
Mark Shui Hu <watermarkhu@gmail.com>
2025-12-01 20:22:05 +0100

    fix test

--- test/converttomatlab.jl
+++ test/converttomatlab.jl
@@ -458,13 +458,13 @@ end
     end
 end
 
-end
-
 
 @testset "convert_matfrostarray Vector{NamedTuple}" begin
     v = NamedTuple[ (a=1, b="x"), (a=2, b="y") ]
-    result = MATFrost._ConvertToMATLAB.convert_matfrostarray(v)
+    result = convert_matfrostarray(v)
     @test result isa MATFrostArrayCell
     @test length(result.values) == 2
     @test result.values[1] isa MATFrostArrayCell || result.values[1] isa MATFrostArrayStruct
+end
+
 end
