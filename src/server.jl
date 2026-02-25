5c331f99e6fa7f9dc59d5765a8b2dbf54ce0d4f0
lennard l Mulder <88686866+lmulder1991@users.noreply.github.com>
2026-02-25 08:31:38 +0100

    Update server.jl
    restore Signature
    
    Signed-off-by: lennard l Mulder <88686866+lmulder1991@users.noreply.github.com>

--- src/server.jl
+++ src/server.jl
@@ -12,17 +12,7 @@ using ..MATFrost._ConvertToMATLAB: _ConvertToMATLAB
 
 struct CallMeta
     fully_qualified_name::String
-    signature::Vector{String}
-    # Inner constructors
-    function CallMeta(fully_qualified_name::String, signature::Vector{String})
-        new(fully_qualified_name, signature)
-    end
-    function CallMeta(fully_qualified_name::String, signature::String)
-        new(fully_qualified_name, [signature])
-    end
-    function CallMeta(fully_qualified_name::String)
-        new(fully_qualified_name, String[])
-    end
+    signature::String 
 end
 CallMeta(fully_qualified_name::String) = CallMeta(fully_qualified_name, "")
 struct MATFrostResultMATLAB{T}
