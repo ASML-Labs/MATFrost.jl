c134b8206676600267742789b547eece65d0261f
Joris Belier <joris.belier@asml.com>
2026-03-12 13:55:15 +0100

    Update server.jl

--- src/server.jl
+++ src/server.jl
@@ -71,7 +71,7 @@ function package_is_loaded(packagename)
     end
 end
 
-package_is_loaded2() = Base.invokelast(package_is_loaded, :MATFrost)
+package_is_loaded2() = Base.invokelatest(package_is_loaded, :MATFrost)
 
 function callsequence(io::IO)
     callstruct = read_matfrostarray!(io)
