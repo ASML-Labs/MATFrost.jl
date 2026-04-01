95e07b9732d902cc55975f351454f885e094da92
jorisbelierasml <joris.belier@asml.com>
2026-04-01 15:25:14 +0200

    fix: Install script
    Signed-off-by: jorisbelierasml <joris.belier@asml.com>

--- src/install.jl
+++ src/install.jl
@@ -1,41 +1,17 @@
 
 function install(destdir=pwd())
 
-# projectname = TOML.parsefile(Base.active_project())["name"]
-
-# projectdirrel = splitpath(Base.active_project())[end-1]
-
-# joinpath(pkgdir(MATFrost), "src", "matlab")
-
-# matdir = joinpath(Base.active_project(), "..", "..", "TEMP_" * projectname)
-
-# cp(joinpath(pkgdir(MATFrost), "src", "matlab"), 
-#     matdir, force=true)
-
-# matfrostjulia = read(joinpath(pkgdir(MATFrost), "src", "matlab", "matfrostjulia.m"), String)
-
-# # The following manipulations will convert base file `matfrostjulia.m` to target environment MATLAB bindings.
-# matfrostjulia = replace(matfrostjulia, "classdef matfrostjulia"                  => "classdef " * projectname)                          # Replace class name.           (Line: 1)
-# matfrostjulia = replace(matfrostjulia, "{{ relative_environment }}"              => projectdirrel)                                      # Replace relative environment. (Line: ~14)
-# matfrostjulia = replace(matfrostjulia, "function obj = matfrostjulia(argstruct)" => ("function obj = " * projectname * "(argstruct)"))  # Replace constructor name.     (Line: ~25)
-
-# write(joinpath(matdir, projectname * ".m"), matfrostjulia)
-
-# rm(joinpath(matdir, "matfrostjulia.m"))
-
-
 if isdir(joinpath(destdir, "@matfrostjulia"))
     rm(joinpath(destdir, "@matfrostjulia"), recursive=true)
 end
 
-cp(joinpath(pkgdir(MATFrost), "src", "matlab"), joinpath(destdir, "@matfrostjulia"))
+mkpath(joinpath(destdir, "@matfrostjulia"))
+
+cp(joinpath(pkgdir(MATFrost), "src", "matlab", "@matfrostjulia"), joinpath(destdir, "@matfrostjulia"), force=true)
 
 foreach(readdir(artifact"matfrost-mex")) do fp
-    cp(joinpath(artifact"matfrost-mex", fp),  joinpath(destdir, "@matfrostjulia", "private", fp))
+    cp(joinpath(artifact"matfrost-mex", fp),  joinpath(destdir, "@matfrostjulia", "private", fp), force=true)
 end
-
-# mv(matdir, joinpath(Base.active_project(), "..", "..", "@" * projectname), force=true)
-
 
 end
 
