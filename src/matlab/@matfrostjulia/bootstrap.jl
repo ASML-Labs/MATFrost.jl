62ac8441642d0d5c9a5645de1b41ec95af30b01e
Mark Shui Hu <watermarkhu@gmail.com>
2026-08-20 15:25:26 +0200

    ci(security): downgrade ubuntu and pin actions (#133)
    * ci: downgrade ubuntu
    
    * pin actions
    
    * comment

--- src/matlab/@matfrostjulia/bootstrap.jl
+++ src/matlab/@matfrostjulia/bootstrap.jl
@@ -4,7 +4,7 @@ The bootstrap script for launching the matfrostserver.
 
 println("Starting MATFrost server")
 
-MATFROST_MATLAB_VERSION = v"0.5.0"
+MATFROST_MATLAB_VERSION = v"0.0.0" # Version is patched automatically during release
 
 try
     using MATFrost
